1429a052f72ec0f3926613553a05421b0d0d4ee9
hackerx20 <shivansh_t@mt.iitr.ac.in>
2025-01-21 13:02:35 +0530

    Added build public file part 4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../public', // Output to a public directory at the root
+  },
   server:{
     port:8000,
     proxy: {
