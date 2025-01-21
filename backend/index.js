eb3bce0a17c51db622bd74c34bc89dfd8326d1b2
hackerx20 <shivansh_t@mt.iitr.ac.in>
2025-01-21 12:24:41 +0530

    Added build public file part 2

--- backend/index.js
+++ backend/index.js
@@ -30,9 +30,9 @@ app.use("/api/users",Userdata);
 app.use("/api/posts",Userpost);
 app.use("/api/notifications",appNotification);
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"/frontend/public")));
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"frontend", "public", "index.html"));
+        res.sendFile(path.resolve(__dirname,"frontend", "dist", "index.html"));
     });
 }
 app.listen(Port, ()=>{
