503d32befd261da61b2ef2a988b3debecb45b687
hackerx20 <shivansh_t@mt.iitr.ac.in>
2025-01-21 12:13:28 +0530

    Added build public file

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,7 @@ cloudinary.config({
 const app = express();
 const Port = process.env.PORT || 5000;
 const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname,"/frontend/public")));
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -29,9 +30,9 @@ app.use("/api/users",Userdata);
 app.use("/api/posts",Userpost);
 app.use("/api/notifications",appNotification);
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.use(express.static(path.join(__dirname,"/frontend/public")));
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname,"frontend", "public", "index.html"));
     });
 }
 app.listen(Port, ()=>{
